600341ecbe6527e2683af93e86482c2e73c273c7
Egor <egor.basharin.03123321@gmail.com>
2025-07-27 15:16:36 +0300

    Пятый коммит

--- recipes/settings.py
+++ recipes/settings.py
@@ -4,3 +4,8 @@ LOGOUT_REDIRECT_URL = '/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [D/Python/recipes/ "static"]
 STATIC_ROOT = D/Python/recipes / 'staticfiles'
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+
